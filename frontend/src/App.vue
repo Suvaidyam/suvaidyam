cfb74c3c08b5323bfdde9d156f88b8fa4afcf6cd
Vishwash singh <singhvishwash776@gmail.com>
2025-05-23 13:51:38 +0000

    worked on frontened pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <Header />
     <router-view />
   </div>
+  <Footer />
 </template>
+<script setup>
+import Footer from './pages/footer.vue';
+import Header from './pages/header.vue';
+
+</script>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,62 +1,7 @@
 <template>
   <div id="app" class="min-h-screen flex flex-col">
     <!-- Header -->
-    <header class="w-full h-20 fixed top-0 bg-white z-[3000] shadow">
-      <div class="w-full h-full max-w-[1720px] mx-auto flex px-5 lg:px-20 justify-between">
-        <div class="h-full flex items-center">
-          <router-link to="/">
-            <img 
-              class="w-1/2 md:w-1/3 lg:w-1/4" 
-              src="../assets/images/suvaidyam logo.png" 
-              alt="Suvaidyam Logo"
-            >
-          </router-link>
-        </div>
-        
-        <div class="h-full flex items-center justify-center">
-          <!-- Desktop Navigation -->
-          <div class="hidden lg:block">
-            <div class="flex gap-8 items-center">
-              <div class="flex gap-6">
-                <router-link 
-                  class="font-semibold text-[15px] hover:text-slate-500 text-blue-900" 
-                  to="/about"
-                >
-                  About Us
-                </router-link>
-                <router-link 
-                  class="font-semibold text-[15px] hover:text-slate-500 text-blue-900" 
-                  to="/work"
-                >
-                  Where We Work
-                </router-link>
-                <router-link 
-                  class="font-semibold text-[15px] hover:text-slate-500 text-blue-900" 
-                  to="/contact"
-                >
-                  Contact Us
-                </router-link>
-              </div>
-              <router-link to="/involved">
-                <button class="w-48 h-10 text-[10px] font-bold border-[1.5px] border-blue-900 hover:bg-blue-900 hover:text-white tracking-widest duration-500">
-                  GET INVOLVED
-                </button>
-              </router-link>
-            </div>
-          </div>
-          
-          <!-- Mobile Menu Button -->
-          <div class="block lg:hidden">
-            <button @click="toggleMobileMenu" class="text-4xl cursor-pointer">
-              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="w-6 h-6">
-                <path fill-rule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-    </header>
-
+      <!-- <Navbar/> -->
     <!-- Main Content -->
     <main class="w-full h-full max-w-[1800px] mx-auto">
       <!-- Hero Carousel -->
@@ -241,132 +186,6 @@
         </div>
       </section>
     </main>
-     
-        
-
-    <!-- Footer -->
-    <footer class="w-full mt-auto">
-  <!-- Main footer content -->
-          <div class="w-full max-w-[1720px] mx-auto h-auto py-7 px-6 md:px-12 lg:px-24 xl:px-30">
-            <div class="flex flex-col gap-3 ">
-              <h3 class="text-[40px] md:text-[46px] font-serif">SUPPORT US</h3>
-              <div class="relative w-[268px] flex justify-end items-center">
-                <span class="h-[1px] bg-blue-700 w-[40px] absolute left-0"></span>
-                <span class="text-right h-6 z-50 font-light text-[16px]">Your support means saved lives</span>
-              </div>
-              <button class="bg-blue-900 text-white text-[10px] tracking-widest border-[1.5px] border-blue-900 w-28 h-9 font-extrabold hover:bg-white hover:text-blue-900 duration-500">
-                DONATE
-              </button>
-        </div>
-            <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-10 lg:gap-24 items-start">
-              <!-- Support Us -->
-              <div class="flex flex-col gap-6">
-              
-                <div class="flex flex-col gap-2 mt-4">
-                  <p class="font-semibold text-lg">Registered Office</p>
-                  <p class="font-serif text-sm font-medium">VILLINTEL SUVAIDYAM PRIVATE LIMITED</p>
-                  <p class="font-light text-sm text-gray-600">
-                    CIN - U62099DL2024PTC436726<br> 
-                    PAN - AAKCV4369C<br> 
-                    TAN - DELV29114B
-                  </p>
-                </div>
-              </div>
-
-              <!-- Address -->
-              <div>
-                <p class="font-semibold text-lg mb-4">Address</p>
-                <div class="flex flex-col gap-3 text-sm text-gray-600">
-                  <p>
-                    F-374/C GF BLOCK -F, AYA NAGAR EXTN PH-6,<br>
-                    South Delhi - 110047
-                  </p>
-                  <p>+91-8308623995</p>
-                  <a href="mailto:tech@suvaidyam.com" class="text-gray-600">
-                    tech@suvaidyam.com
-                  </a>
-                </div>
-              </div>
-
-              <!-- Get Involved -->
-              <div>
-                <p class="font-semibold text-lg mb-4">GET INVOLVED</p>
-                <p class="text-sm text-gray-600">
-                  Let's combine forces to make a<br>difference together
-                </p>
-              </div>
-
-              <!-- Join Us -->
-              <div>
-                <p class="font-semibold text-lg mb-4">JOIN US</p>
-                
-                <div class="flex flex-col gap-4">
-                  <div class="flex gap-3">
-                    <a href="https://www.instagram.com/suvaidyam/" target="_blank" rel="noopener noreferrer">
-                      <div class="w-8 h-8 rounded-full bg-pink-500 flex items-center justify-center text-white">
-                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" class="w-4 h-4">
-                          <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
-                        </svg>
-                      </div>
-                    </a>
-                    <a href="https://whatsapp.com/channel/0029VaAg20iG3R3rCSYYjL30" target="_blank" rel="noopener noreferrer">
-                      <div class="w-8 h-8 rounded-full bg-green-500 flex items-center justify-center">
-                        <svg stroke="currentColor" fill="white" stroke-width="0" viewBox="0 0 448 512" class="w-5 h-5">
-                          <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path>
-                        </svg>
-                      </div>
-                    </a>
-                    <a href="https://www.youtube.com/@suvaidyam/featured" target="_blank" rel="noopener noreferrer">
-                      <div class="w-8 h-8 rounded-full bg-red-500 flex items-center justify-center">
-                        <svg stroke="currentColor" fill="white" stroke-width="0" viewBox="0 0 576 512" class="w-5 h-5">
-                          <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path>
-                        </svg>
-                      </div>
-                    </a>
-                    <a href="https://www.linkedin.com/company/suvaidyam/?originalSubdomain=in" target="_blank" rel="noopener noreferrer">
-                      <div class="w-8 h-8 rounded-full bg-blue-600 flex items-center justify-center text-white">
-                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 16 16" class="w-4 h-4">
-                          <path d="M6 6h2.767v1.418h0.040c0.385-0.691 1.327-1.418 2.732-1.418 2.921 0 3.461 1.818 3.461 4.183v4.817h-2.885v-4.27c0-1.018-0.021-2.329-1.5-2.329-1.502 0-1.732 1.109-1.732 2.255v4.344h-2.883v-9z"></path>
-                          <path d="M1 6h3v9h-3v-9z"></path>
-                          <path d="M4 3.5c0 0.828-0.672 1.5-1.5 1.5s-1.5-0.672-1.5-1.5c0-0.828 0.672-1.5 1.5-1.5s1.5 0.672 1.5 1.5z"></path>
-                        </svg>
-                      </div>
-                    </a>
-                  </div>
-                  
-                  <!-- Newsletter Signup -->
-                  <div class="flex gap-2">
-                    <input 
-                      v-model="email"
-                      name="email" 
-                      type="email" 
-                      placeholder="Enter email address" 
-                      class="border-2 flex-1 px-3 py-2 border-blue-900 placeholder:text-sm placeholder:font-light outline-none text-sm font-light"
-                      required
-                    >
-                    <button 
-                      type="submit" 
-                      class="px-4 py-2 border-2 border-blue-900 hover:bg-blue-900 hover:text-white duration-500 text-xs font-extrabold tracking-widest"
-                    >
-                      JOIN
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-          <!-- Copyright -->
-          <div class="w-full bg-blue-400 h-16">
-            <div class="w-full max-w-[1250px] h-full lg:px-[185px] flex items-center lg:justify-start">
-              <p class="text-white font-light text-sm px-6">
-                © 2023 by Suvaidyam, Saran, Bihar
-              </p>
-            </div>
-          </div>
-    </footer>
-
-    <!-- Scroll to Top Button -->
     <button 
       v-show="showScrollToTop"
       @click="scrollToTop"
@@ -383,6 +202,7 @@
 import { ref, reactive, onMounted, onBeforeUnmount } from 'vue'
 import image1 from '../assets/images/image1.jpg'
 import image2 from '../assets/images/image2.jpg'
+import Navbar from '../pages/header.vue'
 
 // Component name (not required in <script setup>, only useful in options API or defineOptions)
 const currentSlide = ref(0)
@@ -536,8 +356,7 @@ h1 {
 
 /* Focus states for accessibility */
 button:focus,
-a:focus,
-input:focus {
+a:focus{
   outline: 2px solid #3b82f6;
   outline-offset: 2px;
 }
