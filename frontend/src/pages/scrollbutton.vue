f092cf6e3728eaa0f83ceedcc57d6f003c67e123
Vishwash singh <singhvishwash776@gmail.com>
2025-05-27 13:50:22 +0000

    added scroll button style

---
+++ frontend/src/pages/scrollbutton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button 
+    v-show="showScrollToTop" 
+    @click="scrollToTop" 
+    class="fixed right-5 bottom-10 z-50 w-10 h-10  text-white bg-blue-700 rounded-full flex items-center justify-center cursor-pointer duration-500 focus:outline-none "
+  >
+    <svg 
+      stroke="currentColor" 
+      fill="currentColor" 
+      stroke-width="0" 
+      viewBox="0 0 16 16" 
+      class="w-4 h-4"
+    >
+      <path 
+        fill-rule="evenodd" 
+        clip-rule="evenodd" 
+        d="M13.854 7l-5-5h-.707l-5 5 .707.707L8 3.561V14h1V3.56l4.146 4.147.708-.707z"
+      />
+    </svg>
+  </button>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+
+const showScrollToTop = ref(false)
+
+const handleScroll = () => {
+  showScrollToTop.value = window.pageYOffset > 300
+}
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+</script>
